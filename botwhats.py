cbe596a6e2f0221c413bf23fe7e58ee90e90fb82
Mikael Carvalho <mcb3@discente.ifpe.edu.br>
2021-07-30 22:29:15 -0300

    criação do arquivo main

---
+++ botwhats.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import time
+
+
+driver = webdriver.Chrome(executable_path=r"C:\WorkSpace\Vscode\Projetos\bot-whatsapp\chromedriver.exe")
+driver.get('https://www.google.com.br')
+
+
+
+
